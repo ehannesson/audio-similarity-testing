f626d84365ac1833ef4401fc51c79970cd9552e8
Adam Fidler <fidlerja@byu.edu>
2019-11-15 15:59:17 -0700

    audio files and data csvs

--- audio_downloader.py
+++ audio_downloader.py
@@ -7,13 +7,22 @@ import sys
 
 n = int(sys.argv[1])
 
-with open('./data/link_df','rb') as f:
-    df = pickle.load(f)
+with open('./data.csv','r') as f:
+    df = pd.read_csv(f)
 
 for i,link in enumerate(df['link'][n:]):
     time.sleep(1)
-    audio = requests.get(link)
-    filename = link[len(re.sub(r'/[^/]*\.mp3','',link))+1:]
-    with open('./audio_files/' + filename, 'wb+') as f:
-        f.write(audio.content)
-        print('{0}/{1}'.format(i+1,len(df['link'])),filename)
+    try:
+        audio = requests.get(link)
+        filename = link[len(re.sub(r'/[^/]*\.(mp3|wav|wma)','',link))+1:]
+        print(filename,end=' ')
+    except:
+        with open('./link_log.txt','+a') as f:
+            f.write('Error accessing {}th link\n'.format(n+i))
+    try:
+        with open('./audio_files/' + filename, '+wb') as f:
+            f.write(audio.content)
+            print('{0}/{1}'.format(i+1,len(df['link'][n:])))
+    except:
+        with open('./dl_log.txt','+a') as f:
+            f.write('Error reading/saving {}\n'.format(link))
