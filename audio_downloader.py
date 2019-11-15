99d13f1aa56eb31bc02441c63fc9f41c0025e12e
Adam Fidler <fidlerja@byu.edu>
2019-11-15 14:43:50 -0700

    audio_downloader.py command line indexing

--- audio_downloader.py
+++ audio_downloader.py
@@ -3,11 +3,14 @@ import pandas as pd
 import requests
 import time
 import re
+import sys
+
+n = int(sys.argv[1])
 
 with open('./data/link_df','rb') as f:
     df = pickle.load(f)
 
-for i,link in enumerate(df['link']):
+for i,link in enumerate(df['link'][n:]):
     time.sleep(1)
     audio = requests.get(link)
     filename = link[len(re.sub(r'/[^/]*\.mp3','',link))+1:]
